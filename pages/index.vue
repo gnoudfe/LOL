e51d0e8135a75f2b0cee9bcf52e935f758a3adcc
Duong <daiduong020802@gmail.com>
2024-01-16 15:26:30 +0700

    updatee

--- pages/index.vue
+++ pages/index.vue
@@ -44,35 +44,7 @@
 </template>
 
 <script setup>
-import { useAllChampions } from "../../store/useChampion";
-import { scrollToTop } from "../utils/scrollToTop";
-const { state } = useAllChampions();
-if (state.champions.length <= 0) {
-  try {
-    const { data: allChampions } = await useFetch(
-      `https://sprout-ripple-frame.glitch.me/allChampions?_page=1&_limit=${
-        state.itemPerPage
-      }`,
-      {
-        transform: (allChampions) => {
-          return allChampions.map((item) => ({
-            uid: item.uid,
-            champion_name: item.champion_name,
-            champion_splash: item.champion_splash,
-            recommended_roles: item.recommended_roles,
-            difficulty: item.difficulty,
-            champion: item.champion,
-          }));
-        },
-      }
-    );
-    state.champions = allChampions.value;
-  } catch (err) {
-    console.log(err);
-  }
-} else {
-  scrollToTop();
-}
+
 </script>
 
 <style scoped>
